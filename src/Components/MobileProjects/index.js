81ceb4f729c71c66658e6d1b945dc4bda0394a76
thiago-albuquerque <contatothiagoalbuquerque@gmail.com>
2022-09-29 10:27:07 -0300

    Inclusão dos projetos Medite e Interval no componente MobileProjects

--- src/Components/MobileProjects/index.js
+++ src/Components/MobileProjects/index.js
@@ -1,132 +1,202 @@
-import React from 'react';
+import React from "react";
 
 import Slider from "react-slick";
-import "slick-carousel/slick/slick.css"; 
+import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 
-import { FaGithub } from 'react-icons/fa'; 
+import { FaGithub } from "react-icons/fa";
+import { IoLogoGooglePlaystore } from "react-icons/io5";
 
-import { 
-    Container,
-    MobileName,
-    MobileItem,
-    InfoContainer,
-    TitleContainer,
-    Title,
-    SubTitle,
-    ActionContainer,
-    H4,
-    H6,
-    BtnGithub,
-} from './styles';
+import {
+  Container,
+  MobileName,
+  MobileItem,
+  InfoContainer,
+  TitleContainer,
+  Title,
+  SubTitle,
+  ActionContainer,
+  H4,
+  H6,
+  BtnGithub,
+} from "./styles";
 
-export default function MobileProjects(){
+export default function MobileProjects() {
+  const settings = {
+    className: "slides",
+    infinite: true,
+    speed: 500,
+    slidesToShow: 3,
+    initialSlide: 0,
+    responsive: [
+      {
+        breakpoint: 1080,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2,
+          infinite: true,
+        },
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2,
+        },
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        },
+      },
+    ],
+  };
 
-    const settings = {
-        className: 'slides',
-        infinite: true,
-        speed: 500,
-        slidesToShow: 3,
-        initialSlide: 0,
-        responsive: [
-            {
-              breakpoint: 1080,
-              settings: {
-                slidesToShow: 2,
-                slidesToScroll: 2,
-                infinite: true,
-              }
-            },
-            {
-              breakpoint: 600,
-              settings: {
-                slidesToShow: 2,
-                slidesToScroll: 2,
-              }
-            },
-            {
-              breakpoint: 480,
-              settings: {
-                slidesToShow: 1,
-                slidesToScroll: 1
-              }
-            }
-          ]
-    }
+  return (
+    <Container>
+      <MobileName>Projetos Mobile</MobileName>
+      <Slider {...settings}>
+        {/* Medite */}
+        <MobileItem>
+          <InfoContainer>
+            <TitleContainer>
+              <Title>Medite</Title>
+              <SubTitle>Aplicativo de meditação</SubTitle>
+            </TitleContainer>
 
-    return(
-        <Container>
-            <MobileName>Projetos Mobile</MobileName>
-                <Slider {...settings}>
+            <ActionContainer>
+              <H4>Tecnologias</H4>
+              <H6>
+                Javascript | Styled Components | React Native | React Native
+                Sound | React Navigation | NetInfo | Firebase | i18next | AdMob
+              </H6>
 
-                    <MobileItem>
-                        <InfoContainer>
-                            <TitleContainer>
-                                <Title>Cinemando App</Title>
-                                <SubTitle>Aplicativo com informações sobre filmes</SubTitle>
-                            </TitleContainer>
+              <BtnGithub
+                href="https://play.google.com/store/apps/details?id=com.medite"
+                target={"_blank"}
+                rel="noopener noreferrer"
+              >
+                <IoLogoGooglePlaystore />
+              </BtnGithub>
+            </ActionContainer>
+          </InfoContainer>
+        </MobileItem>
 
-                            <ActionContainer>
-                                <H4>Tecnologias</H4>
-                                <H6>
-                                    Expo | Javascript | Styled Components | 
-                                    React Native | Hook | Navigation | FlatList | 
-                                    Dados Offline | Axios | API TMDB
-                                </H6>
+        {/* Interval */}
+        <MobileItem>
+          <InfoContainer>
+            <TitleContainer>
+              <Title>Interval</Title>
+              <SubTitle>
+                Aplicativo para gerenciamento de intervalos de tempo
+              </SubTitle>
+            </TitleContainer>
 
-                                <BtnGithub href='https://github.com/thiago-albuquerque/cinemandoApp' target={'_blank'} rel='noopener noreferrer'>
-                                    <FaGithub/>
-                                </BtnGithub>
-                            </ActionContainer>
-                        </InfoContainer> 
-                    </MobileItem>
+            <ActionContainer>
+              <H4>Tecnologias</H4>
+              <H6>
+                Javascript | Styled Components | React Native | React Native
+                Sound | NetInfo | AdMob
+              </H6>
 
-                    <MobileItem>
-                        <InfoContainer>
-                            <TitleContainer>
-                                <Title>Maria Bonita</Title>
-                                <SubTitle>Aplicativo de moda feminina</SubTitle>
-                            </TitleContainer>
+              <BtnGithub
+                href="https://play.google.com/store/apps/details?id=com.intervalwork"
+                target={"_blank"}
+                rel="noopener noreferrer"
+              >
+                <IoLogoGooglePlaystore />
+              </BtnGithub>
+            </ActionContainer>
+          </InfoContainer>
+        </MobileItem>
 
-                            <ActionContainer>
-                                <H4>Tecnologias</H4>
-                                <H6>
-                                    Expo | Javascript | Styled Components | 
-                                    React Native | Hook | Stack Navigator
-                                </H6>
+        {/* Cinemando */}
+        <MobileItem>
+          <InfoContainer>
+            <TitleContainer>
+              <Title>Cinemando App</Title>
+              <SubTitle>Aplicativo com informações sobre filmes</SubTitle>
+            </TitleContainer>
 
-                                <BtnGithub href='https://github.com/thiago-albuquerque/mariabonita' target={'_blank'} rel='noopener noreferrer'>
-                                    <FaGithub/>
-                                </BtnGithub>
-                            </ActionContainer>
-                        </InfoContainer> 
-                    </MobileItem>
-                    
-                    <MobileItem>
-                        <InfoContainer>
-                            <TitleContainer>
-                                <Title>Short URL</Title>
-                                <SubTitle>Aplicativo encurtador de links</SubTitle>
-                            </TitleContainer>
+            <ActionContainer>
+              <H4>Tecnologias</H4>
+              <H6>
+                Expo | Javascript | Styled Components | React Native | Hook |
+                Navigation | FlatList | Dados Offline | Axios | API TMDB
+              </H6>
 
-                            <ActionContainer>
-                                <H4>Tecnologias</H4>
-                                <H6>Javascript | Styled Components | React Native | Clipboard | API Cuttly</H6>
+              <BtnGithub
+                href="https://github.com/thiago-albuquerque/cinemandoApp"
+                target={"_blank"}
+                rel="noopener noreferrer"
+              >
+                <FaGithub />
+              </BtnGithub>
+            </ActionContainer>
+          </InfoContainer>
+        </MobileItem>
 
-                                <BtnGithub href='https://github.com/thiago-albuquerque/shortURL' target={'_blank'} rel='noopener noreferrer'>
-                                    <FaGithub/>
-                                </BtnGithub>
-                            </ActionContainer>
-                        </InfoContainer> 
-                    </MobileItem>
+        {/* Maria Bonita */}
+        <MobileItem>
+          <InfoContainer>
+            <TitleContainer>
+              <Title>Maria Bonita</Title>
+              <SubTitle>Aplicativo de moda feminina</SubTitle>
+            </TitleContainer>
 
-                    <MobileItem>
-                        <InfoContainer>
-                            <Title style={{textAlign: 'center', margin: 'auto'}}>Breve</Title>
-                        </InfoContainer> 
-                    </MobileItem>
+            <ActionContainer>
+              <H4>Tecnologias</H4>
+              <H6>
+                Expo | Javascript | Styled Components | React Native | Hook |
+                Stack Navigator
+              </H6>
 
-                </Slider>
-        </Container>
-    );
+              <BtnGithub
+                href="https://github.com/thiago-albuquerque/mariabonita"
+                target={"_blank"}
+                rel="noopener noreferrer"
+              >
+                <FaGithub />
+              </BtnGithub>
+            </ActionContainer>
+          </InfoContainer>
+        </MobileItem>
+
+        {/* Short URL */}
+        <MobileItem>
+          <InfoContainer>
+            <TitleContainer>
+              <Title>Short URL</Title>
+              <SubTitle>Aplicativo encurtador de links</SubTitle>
+            </TitleContainer>
+
+            <ActionContainer>
+              <H4>Tecnologias</H4>
+              <H6>
+                Javascript | Styled Components | React Native | Clipboard | API
+                Cuttly
+              </H6>
+
+              <BtnGithub
+                href="https://github.com/thiago-albuquerque/shortURL"
+                target={"_blank"}
+                rel="noopener noreferrer"
+              >
+                <FaGithub />
+              </BtnGithub>
+            </ActionContainer>
+          </InfoContainer>
+        </MobileItem>
+
+        {/* Breve */}
+        <MobileItem>
+          <InfoContainer>
+            <Title style={{ textAlign: "center", margin: "auto" }}>Breve</Title>
+          </InfoContainer>
+        </MobileItem>
+      </Slider>
+    </Container>
+  );
 }
