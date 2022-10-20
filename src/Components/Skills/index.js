8ad2aa2675adb8bbd070a0636db5ed9d5ac7f884
thiago-albuquerque <contatothiagoalbuquerque@gmail.com>
2022-10-20 16:04:57 -0300

    Inclusão de Hard Skills

--- src/Components/Skills/index.js
+++ src/Components/Skills/index.js
@@ -1,47 +1,53 @@
-import React from 'react';
+import React from "react";
 
-import { 
-    Container,
-    SoftContainer,
-    ImgSoftContainer,
-    SoftImg,
-    TextSoftContainer,
-    SoftH1,
-    SoftP,
-    HardContainer,
-    ImgHardContainer,
-    HardImg,
-    TextHardContainer,
-    HardH1,
-    HardP
-} from './styles';
+import {
+  Container,
+  SoftContainer,
+  ImgSoftContainer,
+  SoftImg,
+  TextSoftContainer,
+  SoftH1,
+  SoftP,
+  HardContainer,
+  ImgHardContainer,
+  HardImg,
+  TextHardContainer,
+  HardH1,
+  HardP,
+} from "./styles";
 
-export default function Skills(){
-    return(
-        <Container>
-            <SoftContainer>
-                <ImgSoftContainer>
-                    <SoftImg src={require('../../Assets/soft1.png')}/>
-                    <SoftImg src={require('../../Assets/soft2.png')}/>
-                </ImgSoftContainer>
+export default function Skills() {
+  return (
+    <Container>
+      <SoftContainer>
+        <ImgSoftContainer>
+          <SoftImg src={require("../../Assets/soft1.png")} />
+          <SoftImg src={require("../../Assets/soft2.png")} />
+        </ImgSoftContainer>
 
-                <TextSoftContainer>
-                    <SoftH1>Soft Skills</SoftH1>
-                    <SoftP>Boa comunicação, resiliência, criatividade, pontualidade,  constante busca pelo desenvolvimento profissional e pessoal.</SoftP>
-                </TextSoftContainer>
-            </SoftContainer>
+        <TextSoftContainer>
+          <SoftH1>Soft Skills</SoftH1>
+          <SoftP>
+            Boa comunicação, resiliência, criatividade, pontualidade, constante
+            busca pelo desenvolvimento profissional e pessoal.
+          </SoftP>
+        </TextSoftContainer>
+      </SoftContainer>
 
-            <HardContainer>
-                <TextHardContainer>
-                    <HardH1>Hard Skills</HardH1>
-                    <HardP>HTML | CSS | Javascript | ReactJs | React Native | Styled Components | Git | Axios</HardP>
-                </TextHardContainer>
-                
-                <ImgHardContainer>
-                    <HardImg src={require('../../Assets/hard1.png')}/>
-                    <HardImg src={require('../../Assets/hard2.png')}/>
-                </ImgHardContainer>
-            </HardContainer>
-        </Container>
-    );
+      <HardContainer>
+        <TextHardContainer>
+          <HardH1>Hard Skills</HardH1>
+          <HardP>
+            HTML | CSS | Styled Components | Sass | Javascript | ReactJs | React
+            Native | NextJs | i18next | Git | Figma | Canva
+          </HardP>
+        </TextHardContainer>
+
+        <ImgHardContainer>
+          <HardImg src={require("../../Assets/hard1.png")} />
+          <HardImg src={require("../../Assets/hard2.png")} />
+        </ImgHardContainer>
+      </HardContainer>
+    </Container>
+  );
 }
